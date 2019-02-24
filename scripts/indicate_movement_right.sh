deeb7d56544c9aeceeaec1d5463aa8cd9175279d
michael <michael@morthal.domain>
2019-02-23 22:25:34 -0600

    Added launch files and scripts

---
+++ scripts/indicate_movement_right.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rosservice call --wait /rcvm/indicate_movement "direction:
+  x: 0.0
+  y: -1.0
+  z: 0.0"
---
+++ scripts/indicate_object_right.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rosservice call --wait /rcvm/indicate_object "relative_orientation:
+  x: 0.06645228
+  y: 0.16043
+  z: -0.37686961
+  w: 0.90984373
+  "
